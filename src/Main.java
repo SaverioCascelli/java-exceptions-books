0ab111e290d3fb2f666ab0b46ea6a2171c369dc0
SaverioCascelli <saverio.cascelli@gmail.com>
2023-03-14 16:23:42 +0100

    [add] main menu

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Book test = null;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quanti libri vuoi popolare?");
+
+        int booksNumber = 0;
         try {
-            test = new Book("primo libro", 25, "Gigi", "Gigione");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            test.setAuthor("");
-        } catch (NullPointerException | IllegalArgumentException ex) {
-            System.out.println("Errore nel campo Autore: " + ex.getMessage());
+            booksNumber = Integer.parseInt(scan.nextLine());
+            if (booksNumber < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Input non valido, devi inserire un numero positivo");
         }
 
-        try {
-            test.setPages(0);
-        } catch (IllegalArgumentException ex) {
-            System.out.println("Errore nel campo Pagine: " + ex.getMessage());
+        Book[] bookArr = new Book[booksNumber];
+
+        for (int i = 0; i < bookArr.length; i++) {
+
+            boolean bookDone = false;
+
+            do {
+
+
+                System.out.println("Libro " + (i + 1));
+                System.out.println("Inserisci Autore");
+                String author = scan.nextLine();
+
+                System.out.println("Inserisci Titolo");
+                String title = scan.nextLine();
+
+                System.out.println("Inserisci Editore");
+                String editor = scan.nextLine();
+
+                System.out.println("Inserisci numero di pagine");
+                int pages = 0;
+                try {
+                    pages = Integer.parseInt(scan.nextLine());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Input non valido");
+                }
+
+                Book newBook = null;
+                try {
+                    newBook = new Book(title, pages, author, editor);
+                    bookDone = true;
+                    bookArr[i] = newBook;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } while (!bookDone);
         }
-        System.out.println(test);
+
+        for (int i = 0; i < bookArr.length; i++) {
+            System.out.println(bookArr[i]);
+        }
+
 
     }
 }
